09fa4686cfb985903a6016f3a49360770e674958
Daniel Hernandez <developer@dobermag.com>
2025-07-15 02:46:30 -0600

    feat(plugin): implement initial version of RealFaviconGenerator plugin

---
+++ controllers/faviconsettings/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * View for the Favicon Settings page.
+ */
+?>
+<?php if (!$this->fatalError) : ?>
+
+    <div id="settings-form-container">
+        <?= $this->makePartial('form') ?>
+    </div>
+
+<?php else : ?>
+    <p class="flash-message static error"><?= e(__($this->fatalError)) ?></p>
+    <p><a href="<?= $parentLink ?>" class="btn btn-default"><?= e(__('Return to System Settings')) ?></a></p>
+<?php endif ?>
